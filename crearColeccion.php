c0b70d1e3694a1a5b39f14da61aa16c097098fe9
lferng17 <55236881+lferng17@users.noreply.github.com>
2021-06-13 13:21:10 +0200

    Añadidas visualizaciones Cromos y Colecciones

--- crearColeccion.php
+++ crearColeccion.php
@@ -24,13 +24,13 @@
             <li><a href="verColecciones.php">VER LA COLECCIÓN</a></li>
         </ul>
     </div>
-    <form action="insertarColeccion.php" method="get" name="formColeccion" enctype="multipart/form-data">
+    <form action="insertarColeccion.php" method="post" name="formColeccion" enctype="multipart/form-data">
         Nombre: <br>
         <input type="text" name="nombre"> <br>
         Precio: <br>
         <input type="text" name="precio"> <br>
         Carátula: <br>
-        <input name="caratula" type="file"> <br>
+        <input type="file" name="caratula"> <br>
         <input type="submit" name="subir" value="Enviar">
     </form>
 </body>
