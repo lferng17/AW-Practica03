7cddcebf0387a63b07b9fa6f183f50f8e08f6ff1
MiguelFn4 <61278873+MiguelFn4@users.noreply.github.com>
2021-06-16 10:59:29 +0200

    Update navbar.php

--- navbar.php
+++ navbar.php
@@ -9,7 +9,7 @@
 <body>
     <div id="navegador">
         <ul>
-            <li style="float:left"> <img src = "/AW-Practica03-main/ImagenesServidor/logo.png" width="100" /></li>
+            <li style="float:left"><a href="index.html"><img src = "/AW-Practica03-main/ImagenesServidor/logo.png" width="100" /></a></li>
             <li id="navBar"><a href="crearColeccion.php">CREAR COLECCIÓN</a></li>
             <li id="navBar"><a href="crearCromo.php">CREAR CROMO</a></li>
             <li id="navBar" style="float:right"><a href="funciones/cerrar_sesion.php">CERRAR SESION</a></li>
